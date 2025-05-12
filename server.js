aa7af3f8b04e3c3769f4ad98f7b1b0d00cef378a
108developer <108developer@gmail.com>
2025-05-12 11:08:42 +0530

    vercel_2

--- server.js
+++ server.js
@@ -263,3 +263,5 @@ app.use("/graphql", (req, res, next) => {
 // ──────────────────────────────────────────────────
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
